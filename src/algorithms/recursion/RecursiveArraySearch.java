65598616ecf6f712c59e6f236ab1c31b7cfa4183
dbc2201 <divyanshb22@gmail.com>
2021-02-15 00:42:36 +0530

    create a method to fill array with random values from -99 to 99 including 0, and call the method in the constructor

--- src/algorithms/recursion/RecursiveArraySearch.java
+++ src/algorithms/recursion/RecursiveArraySearch.java
@@ -1,9 +1,18 @@
 package algorithms.recursion;
 
+import java.util.Random;
+
 public class RecursiveArraySearch {
     private int[] array;
 
     public RecursiveArraySearch(int size) {
         this.array = new int[size];
+        fillArray();
+    }
+
+    private void fillArray() {
+        for (int index = 0, size = this.array.length; index < size; index++) {
+            this.array[index] = new Random().nextInt() % 100;
+        }
     }
 }
