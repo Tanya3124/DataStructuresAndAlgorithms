40e0a51688ad4b7433782f7a15d3eb8db6e2d13d
dbc2201 <divyanshb22@gmail.com>
2021-02-15 00:40:07 +0530

    create a constructor for the RecursiveArraySearch class to initialize the array with a size

--- src/algorithms/recursion/RecursiveArraySearch.java
+++ src/algorithms/recursion/RecursiveArraySearch.java
@@ -2,4 +2,8 @@ package algorithms.recursion;
 
 public class RecursiveArraySearch {
     private int[] array;
+
+    public RecursiveArraySearch(int size) {
+        this.array = new int[size];
+    }
 }
