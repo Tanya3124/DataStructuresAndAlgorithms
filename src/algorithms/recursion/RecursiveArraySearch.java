e802f817954f6be25816e23c5035834dd42c053d
dbc2201 <divyanshb22@gmail.com>
2021-02-15 00:43:20 +0530

    override the toString() method for the RecursiveArraySearch class

--- src/algorithms/recursion/RecursiveArraySearch.java
+++ src/algorithms/recursion/RecursiveArraySearch.java
@@ -1,5 +1,6 @@
 package algorithms.recursion;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class RecursiveArraySearch {
@@ -14,5 +15,10 @@ public class RecursiveArraySearch {
         for (int index = 0, size = this.array.length; index < size; index++) {
             this.array[index] = new Random().nextInt() % 100;
         }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(array);
     }
 }
