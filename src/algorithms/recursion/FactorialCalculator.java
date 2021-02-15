d3ffa05cf79add2b63ecbc94b46066155953ba43
dbc2201 <divyanshb22@gmail.com>
2021-02-15 19:04:23 +0530

    input a positive integer from the user and check if it is indeed positive

--- src/algorithms/recursion/FactorialCalculator.java
+++ src/algorithms/recursion/FactorialCalculator.java
@@ -1,8 +1,18 @@
 package algorithms.recursion;
 
+import java.util.Scanner;
+
 public class FactorialCalculator {
     public static void main(String[] args) {
-        System.out.println(calculateFactorial(4));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a positive integer: ");
+        int number = scanner.nextInt();
+        scanner.close();
+        if (number < 0) {
+            System.err.println("Please input a positive integer next time!");
+        } else {
+            System.out.println("Factorial(" + number + ") = " + calculateFactorial(number));
+        }
     }
 
     private static int calculateFactorial(int i) {
