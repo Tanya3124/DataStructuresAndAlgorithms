c7f122a1c8950993c610a627d38d461e33c32c2f
dbc2201 <divyanshb22@gmail.com>
2021-02-15 23:53:49 +0530

    create ArrayListStack class that implements the StackADT

---
+++ src/datastructures/linear/stacks/arrays/ArrayListStack.java
@@ -0,0 +1,25 @@
+package datastructures.linear.stacks.arrays;
+
+import datastructures.linear.stacks.StackADT;
+
+public class ArrayListStack<E> implements StackADT<E> {
+    @Override
+    public boolean push(E item) {
+        return false;
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
