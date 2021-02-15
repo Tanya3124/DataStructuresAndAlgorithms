33fd4045e6b9a5027b69d0b41d42ca508270a60e
dbc2201 <divyanshb22@gmail.com>
2021-02-15 19:08:55 +0530

    complete the method

--- src/algorithms/recursion/FactorialOfLargeIntegers.java
+++ src/algorithms/recursion/FactorialOfLargeIntegers.java
@@ -4,6 +4,9 @@ import java.math.BigInteger;
 
 public class FactorialOfLargeIntegers {
     private static BigInteger calculateFactorial(BigInteger number) {
-        return null;
+        if (number.equals(BigInteger.ONE) || number.equals(BigInteger.ZERO)) {
+            return BigInteger.ONE;
+        }
+        return number.multiply(calculateFactorial(number.subtract(BigInteger.ONE)));
     }
 }
