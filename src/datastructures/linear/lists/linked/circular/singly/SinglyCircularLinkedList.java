bde8859ed8ac2c6031a9cc3bb4590ae1ff34fc46
dbc2201 <divyanshb22@gmail.com>
2021-02-15 23:47:37 +0530

    create a tail reference variable for the singly circular linked list

--- src/datastructures/linear/lists/linked/circular/singly/SinglyCircularLinkedList.java
+++ src/datastructures/linear/lists/linked/circular/singly/SinglyCircularLinkedList.java
@@ -1,8 +1,18 @@
 package datastructures.linear.lists.linked.circular.singly;
 
 import datastructures.linear.lists.ListADT;
+import datastructures.linear.lists.linked.singly.SinglyLinkedList;
+import datastructures.linear.lists.linked.singly.node.Node;
 
 public class SinglyCircularLinkedList<E> implements ListADT<E> {
+
+    private SinglyLinkedList<E> singlyLinkedList;
+    private Node<E> tail;
+
+    public SinglyCircularLinkedList() {
+        this.singlyLinkedList = new SinglyLinkedList<>();
+    }
+
     @Override
     public boolean add(E item) {
         return false;
