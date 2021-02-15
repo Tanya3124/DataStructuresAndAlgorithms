f0c693eedb2426976600d480bc4f429f127d0f8a
dbc2201 <divyanshb22@gmail.com>
2021-02-15 23:43:43 +0530

    implement the ListADT in the SinglyCircularLinkedList

--- src/datastructures/linear/lists/linked/circular/singly/SinglyCircularLinkedList.java
+++ src/datastructures/linear/lists/linked/circular/singly/SinglyCircularLinkedList.java
@@ -1,4 +1,20 @@
 package datastructures.linear.lists.linked.circular.singly;
 
-public class SinglyCircularLinkedList<E> {
+import datastructures.linear.lists.ListADT;
+
+public class SinglyCircularLinkedList<E> implements ListADT<E> {
+    @Override
+    public boolean add(E item) {
+        return false;
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E remove(int index) throws IndexOutOfBoundsException {
+        return null;
+    }
 }
