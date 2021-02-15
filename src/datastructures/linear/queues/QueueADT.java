ddf9e235560cfd7d3ccf1253b154df2795521eb7
dbc2201 <divyanshb22@gmail.com>
2021-02-16 02:18:24 +0530

    add methods for the QueueADT

--- src/datastructures/linear/queues/QueueADT.java
+++ src/datastructures/linear/queues/QueueADT.java
@@ -1,5 +1,11 @@
 package datastructures.linear.queues;
 
+import java.util.NoSuchElementException;
+
 public interface QueueADT<E> {
+
+    boolean enqueue(E item);
+
+    E dequeue() throws NoSuchElementException;
 
 }
