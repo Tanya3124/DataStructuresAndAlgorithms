e89f7f383edabba333a464eded071ad9f462fe89
dbc2201 <divyanshb22@gmail.com>
2021-02-15 23:54:14 +0530

    make the ArrayStack class implement the StackADT

--- src/datastructures/linear/stacks/arrays/ArrayStack.java
+++ src/datastructures/linear/stacks/arrays/ArrayStack.java
@@ -1,4 +1,25 @@
 package datastructures.linear.stacks.arrays;
 
-public class ArrayStack<E> {
+import datastructures.linear.stacks.StackADT;
+
+public class ArrayStack<E> implements StackADT<E> {
+    @Override
+    public boolean push(E item) {
+        return false;
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 }
