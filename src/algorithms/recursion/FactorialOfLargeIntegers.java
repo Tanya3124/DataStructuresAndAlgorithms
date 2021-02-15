800141e3db63629294468510d7ad5718393e5547
dbc2201 <divyanshb22@gmail.com>
2021-02-15 19:08:45 +0530

    create empty method to calculate factorial of large integers

--- src/algorithms/recursion/FactorialOfLargeIntegers.java
+++ src/algorithms/recursion/FactorialOfLargeIntegers.java
@@ -1,4 +1,9 @@
 package algorithms.recursion;
 
+import java.math.BigInteger;
+
 public class FactorialOfLargeIntegers {
+    private static BigInteger calculateFactorial(BigInteger number) {
+        return null;
+    }
 }
