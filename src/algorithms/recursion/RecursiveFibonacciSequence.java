f39e1a0e1d63f03023b2ac0d3f8b3799dfd8b402
dbc2201 <divyanshb22@gmail.com>
2021-02-15 19:58:37 +0530

    the fibonacci term at index 0 is 0, hence return 0

--- src/algorithms/recursion/RecursiveFibonacciSequence.java
+++ src/algorithms/recursion/RecursiveFibonacciSequence.java
@@ -2,6 +2,9 @@ package algorithms.recursion;
 
 public class RecursiveFibonacciSequence {
     private static int findFibonacciTerm(int index) {
+        if (index == 0) {
+            return 0;
+        }
         return 0;
     }
 
