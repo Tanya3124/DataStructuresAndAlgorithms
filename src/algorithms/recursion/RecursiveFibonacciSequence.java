9a9137734fb8a27ec149e977c48b7333285eabfc
dbc2201 <divyanshb22@gmail.com>
2021-02-15 20:00:11 +0530

    input the number of terms in the fibonacci sequence

--- src/algorithms/recursion/RecursiveFibonacciSequence.java
+++ src/algorithms/recursion/RecursiveFibonacciSequence.java
@@ -1,5 +1,7 @@
 package algorithms.recursion;
 
+import java.util.Scanner;
+
 public class RecursiveFibonacciSequence {
     private static int findFibonacciTerm(int index) {
         if (index == 0) {
@@ -12,6 +14,9 @@ public class RecursiveFibonacciSequence {
     }
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number of terms you want in your fibonacci sequence: ");
+        int numberOfTerms = scanner.nextInt();
+        scanner.close();
     }
 }
