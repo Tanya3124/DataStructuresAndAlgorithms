06ad775b65f35224c6f9ce06a9833564a19f7133
dbc2201 <divyanshb22@gmail.com>
2021-02-15 15:12:10 +0530

    create empty method for tower of hanoi

--- src/algorithms/recursion/TowerOfHanoi.java
+++ src/algorithms/recursion/TowerOfHanoi.java
@@ -1,4 +1,13 @@
 package algorithms.recursion;
 
 public class TowerOfHanoi {
+
+
+    public static void main(String[] args) {
+        showSteps(4, 'a', 'b', 'c');
+    }
+
+    private static void showSteps
+            (int numberOfDisks, char source, char via, char destination) {
+    }
 }
