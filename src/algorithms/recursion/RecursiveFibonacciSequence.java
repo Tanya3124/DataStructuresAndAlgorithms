ef74b5081a5eae1e29de0ab27d21bbc8d0ab7915
dbc2201 <divyanshb22@gmail.com>
2021-02-15 19:58:10 +0530

    create a method to find a fibonacci sequence term based on the index and a main() method

--- src/algorithms/recursion/RecursiveFibonacciSequence.java
+++ src/algorithms/recursion/RecursiveFibonacciSequence.java
@@ -1,4 +1,11 @@
 package algorithms.recursion;
 
 public class RecursiveFibonacciSequence {
+    private static int findFibonacciTerm(int index) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
