4cb0196934c98eb35adbf180d8bffa20d731f71f
dbc2201 <divyanshb22@gmail.com>
2021-02-15 23:55:18 +0530

    make the LinkedStack class implement the StackADT

--- src/datastructures/linear/stacks/lists/LinkedStack.java
+++ src/datastructures/linear/stacks/lists/LinkedStack.java
@@ -1,4 +1,25 @@
 package datastructures.linear.stacks.lists;
 
-public class LinkedStack {
+import datastructures.linear.stacks.StackADT;
+
+public class LinkedStack<E> implements StackADT<E> {
+    @Override
+    public boolean push(E item) {
+        return false;
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 }
