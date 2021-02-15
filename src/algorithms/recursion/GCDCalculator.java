9bf69a06f37a7563ca9df64e783d40e777d5d241
dbc2201 <divyanshb22@gmail.com>
2021-02-15 14:39:11 +0530

    create empty method to calculate GCD

---
+++ src/algorithms/recursion/GCDCalculator.java
@@ -0,0 +1,11 @@
+package algorithms.recursion;
+
+public class GCDCalculator {
+    public static void main(String[] args) {
+        calculateGCD(64, 8);
+    }
+
+    private static int calculateGCD(int i, int i1) {
+        return 0;
+    }
+}
