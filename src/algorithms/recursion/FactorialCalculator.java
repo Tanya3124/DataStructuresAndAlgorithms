32a935520075df5e2425a65cd94450537603d5a3
dbc2201 <divyanshb22@gmail.com>
2021-02-15 14:20:33 +0530

    create empty method to calculate factorial of a number

--- src/algorithms/recursion/FactorialCalculator.java
+++ src/algorithms/recursion/FactorialCalculator.java
@@ -1,4 +1,11 @@
 package algorithms.recursion;
 
 public class FactorialCalculator {
+    public static void main(String[] args) {
+        System.out.println(calculateFactorial(4));
+    }
+
+    private static int calculateFactorial(int i) {
+        return 0;
+    }
 }
