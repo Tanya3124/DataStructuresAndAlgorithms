c9f2178dc7ae6df9f33580f433c450cc09d4c445
dbc2201 <divyanshb22@gmail.com>
2021-02-16 21:35:34 +0530

    create class fields for the CircularQueue class

--- src/datastructures/linear/queues/circular/CircularQueue.java
+++ src/datastructures/linear/queues/circular/CircularQueue.java
@@ -2,9 +2,26 @@ package datastructures.linear.queues.circular;
 
 import datastructures.linear.queues.arrays.ArrayQueue;
 
+import java.util.NoSuchElementException;
+
 public class CircularQueue<E> extends ArrayQueue<E> {
+
+    private final int front;
+    private final int rear;
 
     public CircularQueue(int size) {
         super(size);
+        this.front = 0;
+        this.rear = -1;
+    }
+
+    @Override
+    public boolean enqueue(E item) {
+        return super.enqueue(item);
+    }
+
+    @Override
+    public E dequeue() throws NoSuchElementException {
+        return super.dequeue();
     }
 }
