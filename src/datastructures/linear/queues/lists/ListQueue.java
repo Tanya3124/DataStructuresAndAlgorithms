a3d20d23e1e4c780da75339c85a4db8826376ea0
dbc2201 <divyanshb22@gmail.com>
2021-02-16 21:29:35 +0530

    create a linked list to store the element of the queue

--- src/datastructures/linear/queues/lists/ListQueue.java
+++ src/datastructures/linear/queues/lists/ListQueue.java
@@ -2,9 +2,17 @@ package datastructures.linear.queues.lists;
 
 import datastructures.linear.queues.QueueADT;
 
+import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
 public class ListQueue<E> implements QueueADT<E> {
+
+    private LinkedList<E> queue;
+
+    public ListQueue() {
+        this.queue = new LinkedList<>();
+    }
+
     @Override
     public boolean enqueue(E item) {
         return false;
