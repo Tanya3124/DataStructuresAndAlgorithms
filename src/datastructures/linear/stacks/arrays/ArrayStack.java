a7af0fa90c3061f553a9ec585c67ae7ef3247b49
dbc2201 <divyanshb22@gmail.com>
2021-02-16 21:04:44 +0530

    create a final constant variable to store the maximum size of the stack, and initialize it in the constructor

--- src/datastructures/linear/stacks/arrays/ArrayStack.java
+++ src/datastructures/linear/stacks/arrays/ArrayStack.java
@@ -3,6 +3,13 @@ package datastructures.linear.stacks.arrays;
 import datastructures.linear.stacks.StackADT;
 
 public class ArrayStack<E> implements StackADT<E> {
+
+    private final int MAX_SIZE;
+
+    public ArrayStack(int size) {
+        this.MAX_SIZE = size;
+    }
+
     @Override
     public boolean push(E item) {
         return false;
