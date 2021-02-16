081c6460dfc02207adffc688d39955cbc891a6e7
dbc2201 <divyanshb22@gmail.com>
2021-02-16 21:11:42 +0530

    create a doubly linked list to store the elements of the stack

--- src/datastructures/linear/stacks/lists/LinkedStack.java
+++ src/datastructures/linear/stacks/lists/LinkedStack.java
@@ -1,8 +1,16 @@
 package datastructures.linear.stacks.lists;
 
+import datastructures.linear.lists.linked.doubly.DoublyLinkedList;
 import datastructures.linear.stacks.StackADT;
 
 public class LinkedStack<E> implements StackADT<E> {
+
+    private DoublyLinkedList<E> stack;
+
+    public LinkedStack() {
+        this.stack = new DoublyLinkedList<>();
+    }
+
     @Override
     public boolean push(E item) {
         return false;
