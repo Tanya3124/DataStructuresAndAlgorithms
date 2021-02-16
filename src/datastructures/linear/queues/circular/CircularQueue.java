d098e127717498d0f74ce3475a199b92fba45528
dbc2201 <divyanshb22@gmail.com>
2021-02-16 21:38:15 +0530

    implement the circular queue add method

--- src/datastructures/linear/queues/circular/CircularQueue.java
+++ src/datastructures/linear/queues/circular/CircularQueue.java
@@ -6,18 +6,26 @@ import java.util.NoSuchElementException;
 
 public class CircularQueue<E> extends ArrayQueue<E> {
 
-    private final int front;
-    private final int rear;
+    private final int MAX_SIZE;
+    private final Object[] queue;
+    private int front;
+    private int rear;
 
     public CircularQueue(int size) {
         super(size);
+        this.MAX_SIZE = size;
         this.front = 0;
         this.rear = -1;
+        this.queue = new Object[MAX_SIZE];
     }
 
     @Override
     public boolean enqueue(E item) {
-        return super.enqueue(item);
+        if (rear == MAX_SIZE - 1) {
+            rear = -1;
+        }
+        queue[++rear] = item;
+        return true;
     }
 
     @Override
