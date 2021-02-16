bdc7a6ce36034c419e4f1f3c1fc902b91b56ecb1
dbc2201 <divyanshb22@gmail.com>
2021-02-16 21:20:19 +0530

    implement the methods of the QueueADT

--- src/datastructures/linear/queues/arrays/ArrayQueue.java
+++ src/datastructures/linear/queues/arrays/ArrayQueue.java
@@ -1,4 +1,18 @@
 package datastructures.linear.queues.arrays;
 
-public class ArrayQueue<E> {
+import datastructures.linear.queues.QueueADT;
+
+import java.util.NoSuchElementException;
+
+public class ArrayQueue<E> implements QueueADT<E> {
+
+    @Override
+    public boolean enqueue(E item) {
+        return false;
+    }
+
+    @Override
+    public E dequeue() throws NoSuchElementException {
+        return null;
+    }
 }
