f00369f6fad50ba17797861ab12622d8dd31dc2f
dbc2201 <divyanshb22@gmail.com>
2021-03-01 01:09:33 +0530

    create a reference to store the root node

--- src/datastructures/nonlinear/trees/BinaryTree.java
+++ src/datastructures/nonlinear/trees/BinaryTree.java
@@ -1,4 +1,8 @@
 package datastructures.nonlinear.trees;
 
+import datastructures.nonlinear.trees.node.Node;
+
 public class BinaryTree<E> {
+    // reference variable to store the reference of the root node
+    private Node<E> root;
 }
