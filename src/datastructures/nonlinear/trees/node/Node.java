5a03233867c3bc9f2da04fa49b56e678b935c095
dbc2201 <divyanshb22@gmail.com>
2021-03-01 01:04:13 +0530

    create fields to store the reference of the left and the right child nodes

--- src/datastructures/nonlinear/trees/node/Node.java
+++ src/datastructures/nonlinear/trees/node/Node.java
@@ -1,5 +1,12 @@
 package datastructures.nonlinear.trees.node;
 
 public class Node<E> {
+    // data of the tree node
     private E data;
+
+    // reference to the left child node
+    private Node<E> leftChild;
+
+    // reference to the right child node
+    private Node<E> rightChild;
 }
