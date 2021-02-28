2d7d5b0d77cc621c8d7fabc1c9fd76a22669bdad
dbc2201 <divyanshb22@gmail.com>
2021-03-01 01:10:40 +0530

    create a constructor for the BinaryTree class to create an empty BinaryTree

--- src/datastructures/nonlinear/trees/BinaryTree.java
+++ src/datastructures/nonlinear/trees/BinaryTree.java
@@ -5,4 +5,9 @@ import datastructures.nonlinear.trees.node.Node;
 public class BinaryTree<E> {
     // reference variable to store the reference of the root node
     private Node<E> root;
+
+    public BinaryTree() {
+        // create an empty Binary Tree, root is set to null i.e. the root is not present.
+        this.root = null;
+    }
 }
